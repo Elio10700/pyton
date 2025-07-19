8614db77d96f8141fce0f772e6e8cb43150b81d2
Elio <ebamah548@gmail.com>
2025-07-19 21:50:04 +0100

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,14 @@
+#print statments
+
+print ("welcome")
+print (1000)
+print("a =",100 )
+
+print ("name\t age \njack\+15")
+print ("name\t age \njhon\+18")
+print ("name\t age \njames\+13")
+print ("name\t age \nSam\+14")
+print ("name\t age \nbill\+15")
+print ('this is first line',end=" " )   
+print    ("it will be printed  in the same line")
+       
