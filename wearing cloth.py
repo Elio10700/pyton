d5df64682a89b5b71f34a38c246e8800a821c730
Elio <ebamah548@gmail.com>
2025-08-02 18:49:51 +0100

    Add files via upload

---
+++ wearing cloth.py
@@ -0,0 +1,7 @@
+# wearing clothes
+temp = int(input("enter the temperature today"))
+if temp < 20:
+    print("The temperature is less, wear woolen clothes")
+else:
+    print("The temperature is more, wear light and cotton clothes")
+5
