a0dabc1eb2751a3625e516ab81c6e7084c98ede7
Elio <ebamah548@gmail.com>
2025-08-10 19:23:05 +0100

    Add files via upload

---
+++ revese.py
@@ -0,0 +1,8 @@
+ # revers order 
+
+n = int(input("Enter a number: "))
+print("Reversed number order from {n} to 1:")
+for i in range(n,0,-1):
+    print("the number is", i)
+
+print( "the program is finished")
