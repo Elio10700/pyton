e42322e6630d939bc29872a5dc25b44a5af895e7
Elio <ebamah548@gmail.com>
2025-08-10 18:53:15 +0100

    Add files via upload

---
+++ check age .py
@@ -0,0 +1,14 @@
+# Check if age is between 10 and 20 years
+
+# Take age input
+age = int(input("Enter your age: "))
+
+# Check condition
+if 10 <= age <= 20:
+    print("✅ Your age is between 10 and 20 years.")
+else:
+    print("❌ Your age is NOT between 10 and 20 years.")
+# --- IGNORE ---
+# Take age input    
+age = int("Enter your age: =")
+# Check condition 
