086d3cfcd9e696ad0a8089fddd128163648dd29b
Elio <ebamah548@gmail.com>
2025-09-28 18:57:59 +0100

    Add files via upload

---
+++ 12/set 2 operations.py
@@ -0,0 +1,15 @@
+# set operations
+
+s1 = {'red','blue','green','yellow'}
+s2 = {'green','yellow','cyan','purple'}
+
+print(s1)
+print(s2)
+print("union of s1 and s2")
+print(s1.union(s2))
+
+print("intersection of s1 and s2")
+print(s1.intersection(s2))
+
+print("difference of s1 and s2")
+print(s1.difference(s2))
---
+++ 12/set opertaions .py
@@ -0,0 +1,20 @@
+# set operations
+
+s1 = {1,2,3,4,5}
+print(s1)
+
+# mixed set
+s2 = {10,20,30,40,1.5,'hello',(100,200)}
+print(s2)
+
+# set with duplicates
+s3 = {10,10,20,20,30,40,50}
+print(s3)
+
+#add elements
+s3.add(1000)
+print(s3)
+
+# pop first element
+s3.pop()
+print(s3)
